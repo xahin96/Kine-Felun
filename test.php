650c0f1354223249149161f8ad583e2e736273f4
Mustaq Ahmed <33634683+zishans007@users.noreply.github.com>
2017-12-28 22:16:57 +0600

    Zishans upload
    Side menu bar added with all other windows

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+//$x=($_SESSION['ROLE']);
+//echo isset($_SESSION['productList']);
+//echo "$x";
+echo $_SESSION['ROLE'];
+echo $_SESSION['username'];
+ //echo isset($_SESSION['user_id']);
+//echo $_SESSION['productList'][0]['id'];
+//echo "<img src='causalTshirt_Pic/Tshirt4.JPG'>";
+//<img src="0.png" width="60" height="60" align="right">
+?>
